40b8cc49b91df8b82190bd7e4bc74b89676dfda3
jakub-lesinski <jak-les@wp.pl>
2023-07-06 23:23:08 +0200

    Relocete components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,13 +9,12 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
-  hero: Hero = {
-    id:1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id:1,
+  //   name: 'Windstorm'
+  // };
 
   heroes = HEROES;
-
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
